78b96de877dcec63e24d61493cbfc93ed6b13727
NAVEEN DHAKED <naveendhaked12@gmail.com>
2023-07-12 18:36:20 +0530

    first commit

---
+++ Back-end/index.js
@@ -0,0 +1,116 @@
+// const express= require('express');
+// const app= express();
+// app.get("/",(req,resp)=>{
+// resp.send("app is working....")
+// });
+
+// app.listen(5002)
+
+const express = require("express");
+const cors = require("cors");
+require('./db/config');
+const Product = require("./db/Product");
+const User = require("./db/User");
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-comm';
+const app = express();
+
+
+
+app.use(express.json());
+app.use(cors());
+app.post("/register", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err,token) => {
+    if(err){
+      resp.send({result : "something went wrong please try again"});
+    }
+    resp.send({result,auth:token});
+  })
+})
+
+app.post("/login", async (req, resp) => {
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err,token) => {
+        if(err){
+          resp.send({result : "something went wrong please try again"});
+        }
+        resp.send({user,auth:token});
+      })
+      
+    }
+    else {
+      resp.send({ result: 'No user fount' })
+    }
+  }
+  else {
+    resp.send({ result: 'No user fount' })
+  }
+
+})
+
+app.post("/add-product", async (req, resp) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  resp.send(result)
+})
+
+app.get("/products", async (req, resp) => {
+  let products = await Product.find();
+  if (products.length > 0) {
+    resp.send(products);
+  }
+  else {
+    resp.send({ result: "No Products found" });
+  }
+});
+
+app.delete("/product/:id", async (req, resp) => {
+
+  const result = await Product.deleteOne({ _id: req.params.id });
+  resp.send(result);
+})
+
+
+app.get("/product/:id", async (req, resp) => {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    resp.send(result);
+  }
+  else {
+    resp.send({ "result": "No result found" });
+  }
+});
+
+app.put("/product/:id", async (req, resp) => {
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body
+    }
+  )
+  resp.send(result)
+});
+
+app.get("/search/:key", async (req, resp) => {
+  let result = await Product.find({
+    "$or": [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { price: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } }
+    ]
+  });
+
+  resp.send(result);
+})
+
+
+
+app.listen(3006);
